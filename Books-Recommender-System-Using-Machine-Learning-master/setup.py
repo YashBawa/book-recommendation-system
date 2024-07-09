45a4eeba897d9e874b21621ecc500be42894d724
Yash Bawa <90853282+YashBawa@users.noreply.github.com>
2024-07-09 15:20:29 +0530

    Update setup.py

--- Books-Recommender-System-Using-Machine-Learning-master/setup.py
+++ Books-Recommender-System-Using-Machine-Learning-master/setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Books-Recommender-System-Using-Machine-Learning-master"
+AUTHOR_USER_NAME = "YashBawa"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
